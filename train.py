1449c3072f1e9475dc8fcf392ab91e4d2125b823
hikaruian <18826410980@163.com>
2026-09-08 18:49:51 +0800

    fix bugs

--- train.py
+++ train.py
@@ -1193,7 +1193,7 @@ def main():
                 info.local_rank
             ),
             broadcast_buffers=False,
-            find_unused_parameters=True,
+            find_unused_parameters=False,
         )
 
     raw = unwrap(
